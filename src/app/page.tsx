ede9821e590c163cffaa7229c571cc3cb0eb7d8d
Juan Ibarra <jmibarra86@gmail.com>
2024-09-26 18:41:52 -0300

    Defino mi pagina princial como dashboard y redirecciono a la misma cuando se quiere ir a la home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { redirect } from "next/navigation";
 import Authenticated from "./authenticated/page";
 
 export default function Home() {
+    // Redirigir a /dashboard directamente desde el servidor
+    redirect("/dashboard");
+
     return (
         <Authenticated>
             <h1>Home</h1>
